138340d537e8e9ddac7e66e3e436479961c1b373
congjiajia_007@hotmail.com <congjiajia_007@hotmail.com>
2020-09-28 21:32:17 +0800

    tjwj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'login', loadChildren: () => import('./modules/login/login.module').then(mod => mod.LoginModule)
+  },
+  {
+    path: '**', redirectTo: 'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent
+{
   title = 'TruckAccounts';
+  size: any = 20;
 }
